4f9c185d45822c828575e75f63ecc39a664a2e11
Ammar Al-Khawaldeh <me@ammar.dev>
2022-04-10 02:21:15 +0300

    Prepare Vue & tailwind

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,8 @@
+import { createApp, h } from 'vue'
+import RootComponent from './Components/Root.vue'
+
+
 window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+createApp(RootComponent).mount('#app')
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
     </head>
-    <body class="antialiased">
-        <div id="app"></div>
+    <body class="antialiased h-full">
+        <div id="app" class="flex flex-col min-h-full bg-slate-200"></div>
+        
+        <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
