c6da9fc4c7d7f02c8a9d1dfe3425c76bac6f55b3
Ammar Al-Khawaldeh <me@ammar.dev>
2022-04-09 03:20:14 +0300

    Add seeder for testing

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Ingredient;
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $product = Product::create([
+            'name' => 'Burger',
+            'description' => 'Very delicious beef burger with cheese and onion ',
+            'image' => 'product1.jpg',
+        ]);
+
+        Ingredient::create([
+            'name' => 'Beef',
+            'inserted_amount' => 20000,
+            'current_amount' => 20000,
+        ])->products()->attach($product, ['needed_amount' => 150]);
+
+        Ingredient::create([
+            'name' => 'Cheese',
+            'inserted_amount' => 5000,
+            'current_amount' => 5000,
+        ])->products()->attach($product, ['needed_amount' => 30]);
+
+        Ingredient::create([
+            'name' => 'Onion',
+            'inserted_amount' => 1000,
+            'current_amount' => 1000,
+        ])->products()->attach($product, ['needed_amount' => 20]);
+    }
+}
