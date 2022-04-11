0a2cc4480b521abc63f24aef30ef3cd881e505d6
Ammar Al-Khawaldeh <me@ammar.dev>
2022-04-12 02:27:14 +0300

    Add prices to the grid

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,6 +18,7 @@ class ProductController extends Controller
             'id',
             'name',
             'image',
+            'price',
         ])->paginate();
     }
 
--- resources/js/Components/Products/ProductCard.vue
+++ resources/js/Components/Products/ProductCard.vue
@@ -2,7 +2,12 @@
     <div class="bg-white rounded-lg shadow-md overflow-hidden">
         <img :src="product.image" class="w-full" height="200" width="300">
         <div class="flex flex-col p-4">
-            <h2 class="text-lg pb-2 text-slate-800 font-semibold">{{product.name}}</h2>
+            <div class="flex justify-between">
+                <h2 class="text-lg pb-2 text-slate-800 font-semibold">{{product.name}}</h2>
+                <span class="font-bold text-slate-600">
+                    {{product.price/100}}$
+                </span>
+            </div>
             <button class="bg-violet-500 rounded-lg py-3 text-white font-bold" @click="openProductDetails">
                 Details
             </button>
